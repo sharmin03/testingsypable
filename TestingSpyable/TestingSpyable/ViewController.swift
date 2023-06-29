9e51a71d95cb613e54de446fe8b7d2a452157210
Sharmin Khan <sharmin.khan@moia.io>
2023-06-29 13:49:47 +0200

    Setup empty xcode project

---
+++ TestingSpyable/TestingSpyable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestingSpyable
+//
+//  Created by Sharmin Khan on 29.06.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
